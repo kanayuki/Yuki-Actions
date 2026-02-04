8fee1e6c334b48c3d302bce967ae676bc15a666f
kanayuki <kanayuki@163.com>
2026-02-05 02:37:47 +0800

    action: run proxy/main.py script

--- proxy/main.py
+++ proxy/main.py
@@ -3,11 +3,12 @@ import gen_share_links
 
 
 def main():
-    # 合并订阅链接
-    merge.main()
-    
+
     # 生成分享链接
     gen_share_links.main()
+
+    # 合并订阅链接
+    merge.main()
 
 
 if __name__ == "__main__":
